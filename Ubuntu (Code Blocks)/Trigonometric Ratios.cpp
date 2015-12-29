3b6eb351650b5e3779405421bbb7233370eb132f
StardustGogeta <liam.gerst@gmail.com>
2015-12-28 19:00:26 -0500

    Compacting over 1.5 KB to less than 600 B.
    Fixing up the older C++ tools before attempting to convert them to Python.

--- Ubuntu (Code Blocks)/Trigonometric Ratios.cpp
+++ Ubuntu (Code Blocks)/Trigonometric Ratios.cpp
@@ -2,50 +2,25 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 int main ()
 {
-    double pi = atan(1) * 4;
-    string angleType;
-    cout << "Degrees (d) or radians (r)?\n";
-    cin >> angleType;
-    double orig;
-    cout << "What is the angle measure? (decimal)\n";
-    cin >> orig;
-    if (angleType == "d")
+    double rad,o,pi=atan(1)*4,angle;
+    cout<<"Degrees (0) or radians (1)?\n";
+    cin>>angle;
+    cout<<"What is the angle measure?\n";
+    cin>>o;
+    if (angle==0)
     {
-        orig = orig * pi/180;
-        cout << "sin θ = " << sin(orig) << endl;
-        cout << "cos θ = " << cos(orig) << endl;
-        cout << "tan θ = " << tan(orig) << endl;
-        cout << "csc θ = " << 1/sin(orig) << endl;
-        cout << "sec θ = " << 1/cos(orig) << endl;
-        cout << "cot θ = " << 1/tan(orig) << endl;
+        o=o*pi/180;
     }
     else
     {
-        bool radType;
-        cout << "Is it in terms of pi? (Y=1)\n";
-        cin >> radType;
-        if (radType == true)
+        cout<<"Is it in terms of pi? (Y=1)\n";
+        cin>>rad;
+        if (rad==1)
         {
-            orig = pi * orig;
-            cout << "sin θ = " << sin(orig) << endl;
-            cout << "cos θ = " << cos(orig) << endl;
-            cout << "tan θ = " << tan(orig) << endl;
-            cout << "csc θ = " << 1/sin(orig) << endl;
-            cout << "sec θ = " << 1/cos(orig) << endl;
-            cout << "cot θ = " << 1/tan(orig) << endl;
-        }
-        else
-        {
-            cout << "sin θ = " << sin(orig) << endl;
-            cout << "cos θ = " << cos(orig) << endl;
-            cout << "tan θ = " << tan(orig) << endl;
-            cout << "csc θ = " << 1/sin(orig) << endl;
-            cout << "sec θ = " << 1/cos(orig) << endl;
-            cout << "cot θ = " << 1/tan(orig) << endl;
+            o=pi*o;
         }
     }
-    return 0;
+    cout<<"sin = "<<sin(o)<<"\ncos = "<<cos(o)<<"\ntan = "<<tan(o)<<"\ncsc = "<<1/sin(o)<<"\nsec = "<<1/cos(o)<<"\ncot = "<<1/tan(o)<<endl;
 }
