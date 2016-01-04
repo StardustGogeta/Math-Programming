d5b133c8ad32c95e1c85a3c24f66cb4a1048dc7d
StardustGogeta <liam.gerst@gmail.com>
2016-01-04 17:18:20 -0500

    Update Line Intercept Calculator.py
    Adding in exception handlers with error codes, and reformatting the returned values to strings, not lists.

--- Ubuntu (Code Blocks)/Line Intercept Calculator.py
+++ Ubuntu (Code Blocks)/Line Intercept Calculator.py
@@ -1,15 +1,22 @@
 def calcslope(A,B,C,D,E,F,G,H):
-    s=(D-B)/(C-A)
-    s2=(H-F)/(G-E)
-    y1=B-(s*A)
-    y2=F-(s2*E)
-    x=(y2-y1)/(s-s2)
-    y3=s*x+y1
-    return("The point of intersection is (",x,",",y3,").")
+    try:
+        s=(D-B)/(C-A)
+        s2=(H-F)/(G-E)
+        y1=B-(s*A)
+        y2=F-(s2*E)
+        x=(y2-y1)/(s-s2)
+        y3=s*x+y1
+        return("The point of intersection is ("+str(round(x,3))+", "+str(round(y3,3))+").")
+    except(ZeroDivisionError):
+        return("Error 001: Vertical or parallel lines detected.")
 
 def convert(s,s2,y1,y2):
-    x=(y2-y1)/(s-s2)
-    y3=s*x+y1
-    return("The point of intersection is (",x,",",y3,").")
+    try:
+        x=(y2-y1)/(s-s2)
+        y3=s*x+y1
+        return("The point of intersection is ("+str(round(x,3))+", "+str(round(y3,3))+").")
+    except(ZeroDivisionError):
+        return("Error 002: Vertical or parallel lines detected.")
 
+    
 #convert(int(input("Enter coordinate pairs for two lines (0) or equations (1).\n")))
