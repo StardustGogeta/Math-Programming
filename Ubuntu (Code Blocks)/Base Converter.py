b358dd9f79d698d630fbf7f4b1a7868dc24b76c4
StardustGogeta <liam.gerst@gmail.com>
2016-01-19 16:15:31 -0500

    Vastly improved Base Converter, but still needs error message documentation.

--- Ubuntu (Code Blocks)/Base Converter.py
+++ Ubuntu (Code Blocks)/Base Converter.py
@@ -1,29 +1,20 @@
 from math import *
 
 def convert(fN,fB,sB):
-    if int(fB)>10 or int(fB)<2 or int(sB)>10 or int(sB)<2:
+    try:
+        tVT=int(str(fN),fB)
+        sN=""
+        print(tVT)
+        while tVT:
+            sN+=str(tVT%sB)
+            print(sN)
+            tVT//=sB
+            print(tVT)
+        return(sN[::-1])
+    except ValueError:
         return("Stop wasting time.")
-    else:
-        tVT=0
-        while fN>0:
-            C=fN
-            d=int(floor(log10(C)))
-            C/=10**d
-            dP=10**d
-            dV=fB**d
-            tV=int(C)*dV
-            fN-=int(C)*dP
-            tVT+=tV
-        sN=0
-        tVTN=1
-        while tVTN>0:
-            Z=int(floor(log10(tVT)/log10(sB)))
-            dV=sB**Z
-            tVTN=int(tVT)%dV
-            sN+=(tVT-tVTN)*10**Z/dV
-            tVT=tVTN
-        return(int(sN))
 
 #convert(int(input("What is the number?\n")),
 #          int(input("What is the current base? (1 < x < 11)\n")),
 #          int(input("What is the desired base? (1 < x < 11)\n")))
+#11110100001000111111 is 999999 in base 10
