3fee9a74581875cb761e3ad62ec579a5d948fb54
StardustGogeta <liam.gerst@gmail.com>
2016-01-19 21:33:39 -0500

    Update Error Log.py

--- Ubuntu (Code Blocks)/Error Log.py
+++ Ubuntu (Code Blocks)/Error Log.py
@@ -1,2 +1,3 @@
 Error 001 - ZeroDivisionError caused by vertical or parallel lines in LIC.calcslope
 Error 002 - ZeroDivisionError caused by vertical or parallel lines in LIC.convert
+Error 003 - ValueError caused by an invalid input number or base in BC.convert
