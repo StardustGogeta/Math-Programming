b66e4985c78d5edea9a6bd920ed5518a4790e1f3
StardustGogeta <liam.gerst@gmail.com>
2016-02-28 17:51:04 -0500

    Cutting down the Base Converter further than ever before (in C++)

--- Windows (VS 2015)/Base Converter.cpp
+++ Windows (VS 2015)/Base Converter.cpp
@@ -1,118 +1,31 @@
-// Base Converter.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "What is the number?\n";
-	long firstNumber;
-	cin >> firstNumber;
-	cout << "What is the current base? (Between 2 and 10)\n";
-	int firstBase;
-	cin >> firstBase;
-	cout << "What is the desired base? (Between 2 and 10)\n";
-	int secondBase;
-	cin >> secondBase;
-	if (firstBase < 2)
+	cout << "What is the number?\n";				string fN;	cin >> fN;
+	cout << "What is the current base? (<=36)\n";	int fB;		cin >> fB;
+	cout << "What is the desired base? (<=10)\n";	int sB;		cin >> sB;
+	if (1<fB<37 && 1<sB<11)
 	{
-		cout << "\nPlease stop wasting my time.\n";
+		long tVT = stoi(fN, nullptr, fB);
+		string sN;
+		while (tVT)
+		{
+			sN += to_string(tVT%sB);
+			tVT = floor(tVT / sB);
+		}
+		cout << "\nYour number is ";
+		for (string::reverse_iterator rit = sN.rbegin(); rit != sN.rend(); ++rit)
+			cout << *rit;
+		cout << ".\n";
 	}
 	else
 	{
-		long Counter;
-		long Digit = 0;
-		long DigitTotal;
-		long trueValue;
-		long trueValueTotal = 0;
-		long digitValue = 1;
-		long digitPower;
-		while (firstNumber > 0)
-		{
-			Counter = firstNumber;
-			trueValue = 1;
-			// Figuring out the first digit and its position.
-			while (Counter >= 10)
-			{
-				Counter /= 10;
-				Digit++;
-			}
-			DigitTotal = Digit;
-			digitPower = 1;
-			// Figuring out how many zeroes are behind it, for later. This is when ^ comes in handy in the real world.
-			while (DigitTotal > 0)
-			{
-				digitPower *= 10;
-				DigitTotal--;
-			}
-			// Figuring out how much that digit position is "worth."
-			digitValue = 1;
-			while (Digit > 0)
-			{
-				digitValue *= firstBase;
-				Digit--;
-			}
-			// Obtaining the true value of the digit and subtracting it from the original, in order to repeat again.
-			trueValue *= Counter * digitValue;
-			firstNumber -= Counter * digitPower;
-			trueValueTotal += trueValue;
-			// Code used for debugging issues with the variables.
-			// cout << endl << "digitValue = "<< digitValue;
-			// cout << endl << "trueValue = " << trueValue;
-			// cout << endl << "trueValueTotal = " << trueValueTotal;
-			// cout << endl << "firstNumber = " << firstNumber;
-			// cout << endl;
-		}
-		// 1011 in base 2 is 11 in base 10 and 21 in base 5.
-		long trueValueTotalNew = 1;
-		long secondNumber;
-		long finalSolution = 0;
-		while (trueValueTotalNew > 0)
-		{
-			long secondBasePower = 1;
-			Digit = 0;
-			// Finding the correct digit with a high enough value to encapsulate the number.
-			while (secondBasePower <= trueValueTotal)
-			{
-				secondBasePower *= secondBase;
-				Digit++;
-				// cout << endl << "Digit = " << Digit;
-				// cout << endl << "secondBasePower = " << secondBasePower;
-			}
-			secondBasePower /= secondBase;
-			// Finding the remainder.
-			trueValueTotalNew = trueValueTotal % secondBasePower;
-			digitPower = 1;
-			// Figuring out how many zeroes are behind it, for later. This is when ^ comes in handy in the real world.
-			while (Digit > 1)
-			{
-				digitPower *= 10;
-				Digit--;
-			}
-			// cout << endl << "digitPower = " << digitPower;
-			secondNumber = (trueValueTotal - trueValueTotalNew) * digitPower / secondBasePower;
-			trueValueTotal = trueValueTotalNew;
-			finalSolution += secondNumber;
-			// cout << endl << "trueValueTotal = " << trueValueTotal;
-			// cout << endl << "trueValueTotalNew = " << trueValueTotalNew;
-			// cout << endl << "secondNumber = " << secondNumber;
-			// cout << endl << "finalSolution = " << finalSolution;
-		}
-		cout << endl << "Your desired number is " << finalSolution << "." << endl;
+		cout << "\nStop wasting time.\n";
 	}
 }
-
-/*
-OOOOOO         CCCCCC    M MMM  MMMM   R RRRRRRR
-O     OO     CC          MM    M    M  RR       R
-O      O    C            M     M    M  R
-O      O    C            M     M    M  R
-O     OO    C            M     M    M  R
-OOOOOO        CCCCCCCC   M     M    M  R
-O
-O
-O
-*/
