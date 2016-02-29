d45618d8476c2a2b276b70da7a3daf473289abaa
StardustGogeta <liam.gerst@gmail.com>
2016-02-29 17:28:23 -0500

    Completely redesigned the Visual Basic base converter to make it much shorter.

--- Windows (VS 2015)/Base Converter (Basic).vb
+++ Windows (VS 2015)/Base Converter (Basic).vb
@@ -1,63 +1,24 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("What is the number? (Positive)")
-        Dim firstNumber As Long = Console.ReadLine()
-        Console.WriteLine("What is the current base? (Between 2 and 10)")
-        Dim firstBase As Int16 = Console.ReadLine()
-        Console.WriteLine("What is the desired base? (Between 2 and 10)")
-        Dim secondBase As Int16 = Console.ReadLine()
-        If firstBase < 2 Or firstBase > 10 Or secondBase < 2 Or secondBase > 10 Then
+        Console.WriteLine("What is the number?")
+        Dim fN As Long = Console.ReadLine()
+        Console.WriteLine("What is the current base? (1<x<11)")
+        Dim fB As Int16 = Console.ReadLine()
+        Console.WriteLine("What is the desired base? (1<x<11)")
+        Dim sB As Int16 = Console.ReadLine()
+        If 2 < fB < 10 And 2 < sB < 10 Then
+            Dim tVT As Long = Convert.ToInt64(fN, fB)
+            Dim sN As String = ""
+            While tVT
+                sN += Str(tVT Mod sB)
+                tVT = Math.Floor(tVT / sB)
+            End While
             Console.Write(Environment.NewLine)
-            Console.WriteLine("Please stop wasting my time.")
+            Console.WriteLine("Your desired number is {0}.", Replace(StrReverse(sN), " ", ""))
+            Console.Write(Environment.NewLine)
         Else
-            Dim Counter As Long
-            Dim Digit As Long = 0
-            Dim trueValue As Long
-            Dim trueValueTotal As Long = 0
-            Dim digitValue As Long = 1
-            Dim digitPower As Long
-            While firstNumber > 0
-                Digit = 0
-                Counter = firstNumber
-                trueValue = 1
-                'Figuring out the first digit and its position.
-                While Counter >= 10
-                    Counter /= 10
-                    Digit += 1
-                End While
-                digitPower = (10 ^ Digit)
-                'Figuring out how much that digit position is "worth."
-                digitValue = (firstBase ^ Digit)
-                'Obtaining the true value of the digit and subtracting it from the original, in order to repeat again.
-                trueValue *= Counter * digitValue
-                firstNumber -= Counter * digitPower
-                trueValueTotal += trueValue
-            End While
-            Dim trueValueTotalNew As Long = 1
-            Dim secondNumber As Long
-            Dim finalSolution As Long = 0
-            While trueValueTotalNew > 0
-                Dim secondBasePower As Long = 1
-                Digit = 0
-                'Finding the correct digit with a high enough value to encapsulate the number.
-                While secondBasePower <= trueValueTotal
-                    secondBasePower *= secondBase
-                    Digit += 1
-                End While
-                secondBasePower /= secondBase
-                'Finding the remainder.
-                trueValueTotalNew = trueValueTotal Mod secondBasePower
-                digitPower = (10 ^ (Digit - 1))
-                secondNumber = (trueValueTotal - trueValueTotalNew) * digitPower / secondBasePower
-                trueValueTotal = trueValueTotalNew
-                finalSolution += secondNumber
-            End While
-            Console.Write(Environment.NewLine)
-            Console.Write("Your desired number is ")
-            Console.Write(finalSolution)
-            Console.Write(".")
-            Console.Write(Environment.NewLine)
+            Console.WriteLine("Please stop wasting my time.")
         End If
     End Sub
 
