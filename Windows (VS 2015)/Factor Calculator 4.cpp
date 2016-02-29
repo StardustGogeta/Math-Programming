51ef2560caa11b523b377255f1f2aeb2fbc10dfc
StardustGogeta <liam.gerst@gmail.com>
2016-02-29 17:39:12 -0500

    Fixing up the Factor Calculator in C++

--- Windows (VS 2015)/Factor Calculator 4.cpp
+++ Windows (VS 2015)/Factor Calculator 4.cpp
@@ -8,30 +8,21 @@ using namespace std;
 int main()
 {
 	cout << "What is your number?\n";
-	__int64 inputNumber;
-	cin >> inputNumber;
-	// Asking for the original number and checking that it is a legal one.
-	if (inputNumber > 0)
+	__int64 n;
+	cin >> n;
+	if (n)
 	{
-		for (__int64 Divisor = 1; Divisor < sqrt(inputNumber); Divisor++)
-		/* You might be wondering, why square root?
-		It is the perfect solution to the problem because
-		all factors will be either under the square root or multiply with one below it,
-		so there is no use in brute-forcing it. It will check every integer divisor up until
-		that point, including the counterparts along the way. It can only calculate the factors
-		of 64-bit numbers, so you cannot mash numbers and expect to get an answer. */
+		for (__int64 Divisor = 1; Divisor < sqrt(n); Divisor++)
 		{
-			if (inputNumber % Divisor == 0)
+			if (n % Divisor == 0)
 			{
-				cout << inputNumber / Divisor << "\n" << Divisor << "\n";
+				cout << n / Divisor << "\n" << Divisor << "\n";
 			}
 		}
-		cout << endl << endl << "The list of factors has been successfully generated.\n";
+		cout << "\nThe list of factors has been successfully generated.\n";
 	}
 	else
 	{
-		cout << "Are you trying to kill me?\n" << "You will pay for that.\n";
+		cout << "Sorry, I can't do that.\n";
 	}
-    return 0;
 }
-
