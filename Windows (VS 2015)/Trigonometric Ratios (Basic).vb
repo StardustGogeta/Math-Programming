aaaa6d6772edae7620299c723a4a25c72585ca3d
StardustGogeta <liam.gerst@gmail.com>
2016-03-01 18:38:12 -0500

    Greatly shortening the TR calculator in VB

--- Windows (VS 2015)/Trigonometric Ratios (Basic).vb
+++ Windows (VS 2015)/Trigonometric Ratios (Basic).vb
@@ -2,54 +2,20 @@
 
     Sub Main()
         Dim pi As Double = 3.14159265
-        Console.WriteLine("Degrees (d) or radians (r)?")
-        Dim angleType As String = Console.ReadLine()
-        Console.WriteLine("What is the angle measure? (decimal)")
-        Dim orig As Double = Console.ReadLine()
-        If (angleType = "d") Then
-            Console.WriteLine("sin = ")
-            Console.WriteLine(Math.Sin(orig * 3.14159 / 180))
-            Console.WriteLine("cos = ")
-            Console.WriteLine(Math.Cos(orig * 3.14159 / 180))
-            Console.WriteLine("tan = ")
-            Console.WriteLine(Math.Tan(orig * 3.14159 / 180))
-            Console.WriteLine("csc = ")
-            Console.WriteLine(1 / Math.Sin(orig * 3.14159 / 180))
-            Console.WriteLine("sec = ")
-            Console.WriteLine(1 / Math.Cos(orig * 3.14159 / 180))
-            Console.WriteLine("cot = ")
-            Console.WriteLine(1 / Math.Tan(orig * 3.14159 / 180))
+        Console.WriteLine("Degrees (0) or radians (1)?")
+        Dim ang As Double = Console.ReadLine()
+        Console.WriteLine("What is the angle measure?")
+        Dim o As Double = Console.ReadLine()
+        If ang = 0 Then
+            o = o * pi / 180
         Else
-            Console.WriteLine("Is it in terms of pi? (Y=1)")
-            Dim radType As Boolean = Console.ReadLine()
-            If (radType = True) Then
-                Console.WriteLine("sin = ")
-                Console.WriteLine(Math.Sin(orig * 3.14159))
-                Console.WriteLine("cos = ")
-                Console.WriteLine(Math.Cos(orig * 3.14159))
-                Console.WriteLine("tan = ")
-                Console.WriteLine(Math.Tan(orig * 3.14159))
-                Console.WriteLine("csc = ")
-                Console.WriteLine(1 / Math.Sin(orig * 3.14159))
-                Console.WriteLine("sec = ")
-                Console.WriteLine(1 / Math.Cos(orig * 3.14159))
-                Console.WriteLine("cot = ")
-                Console.WriteLine(1 / Math.Tan(orig * 3.14159))
-            Else
-                Console.WriteLine("sin = ")
-                Console.WriteLine(Math.Sin(orig))
-                Console.WriteLine("cos = ")
-                Console.WriteLine(Math.Cos(orig))
-                Console.WriteLine("tan = ")
-                Console.WriteLine(Math.Tan(orig))
-                Console.WriteLine("csc = ")
-                Console.WriteLine(1 / Math.Sin(orig))
-                Console.WriteLine("sec = ")
-                Console.WriteLine(1 / Math.Cos(orig))
-                Console.WriteLine("cot = ")
-                Console.WriteLine(1 / Math.Tan(orig))
+            Console.WriteLine("In terms of pi (1) or not (0)?")
+            Dim rad As Double = Console.ReadLine()
+            If rad = 1 Then
+                o = pi * o
             End If
         End If
+        Console.WriteLine("sin = {0}{6}cos = {1}{6}tan = {2}{6}csc = {3}{6}sec = {4}{6}cot = {5}", Math.Sin(o), Math.Cos(o), Math.Tan(o), 1 / Math.Sin(o), 1 / Math.Cos(o), 1 / Math.Tan(o), Environment.NewLine)
     End Sub
 
 End Module
