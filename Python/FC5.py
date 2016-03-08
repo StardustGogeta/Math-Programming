8668efff36d6b2c957729a31e5d8d3f8b1a390a3
StardustGogeta <liam.gerst@gmail.com>
2016-03-07 20:56:27 -0500

    Finally adding an FC5!
    You have waited so long for this... Already prepared for a GUI, this new Factor Calculator edition can be many times faster than the old one, marking one of the biggest-ever performance improvements between commits. This can also sort the list of numbers and automatically checks that numbers are exactly precise, to the single digit! Thanks to these fixes, the FC5 is faster than ever, and I can begin work on versions for the other programming languages. Thanks for this and many other, previous code tricks goes out to the ever-generous people of Stack Exchange.

---
+++ Python/FC5.py
@@ -0,0 +1,22 @@
+from decimal import *
+import math
+
+def convert(n):
+    getcontext().prec = math.floor(math.log10(n))
+    numlist=[1]
+    while n-1:
+            f = int(findFactor(n))
+            n /= f
+            numlist+=[f*x for x in numlist]
+            numlist=list(set(numlist))
+    numlist.sort()
+    return(numlist)
+
+def findFactor(n):
+    for d in range(2, math.ceil(Decimal(n).sqrt())):
+        if (n % d == 0):
+            return(d)
+    return(n)
+
+#getcontext().prec = 500
+#convert(Decimal(input("What is the number?\n")))
