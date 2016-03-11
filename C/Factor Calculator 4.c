7152e0dc7d88b6c8d1215799cc545715042ab7e4
StardustGogeta <liam.gerst@gmail.com>
2016-03-11 15:15:42 -0500

    Fixing up the FC4 for C before the FC5

---
+++ C/Factor Calculator 4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    long long n;
+	puts("What is your number?");
+	scanf("%lld",&n);
+	if (n>0)
+	{
+	    long long d;
+		for (d=1; d<=sqrt(n); d++)
+		{
+			if (n % d == 0)
+			{
+				printf("%lld, %lld\n",n/d,d);
+			}
+		}
+		puts("\nThe list of factors has been successfully generated.");
+	}
+	else
+	{
+		puts("\nSorry, I can't do that.");
+	}
+}
