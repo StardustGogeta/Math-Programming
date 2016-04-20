21a75d658ed43bfd5d9c612bf0dd183cbce637d0
StardustGogeta <liam.gerst@gmail.com>
2016-04-20 15:03:26 -0400

    Adding some explanation to the testing document for KS

--- KerbalSpace/Test.ks
+++ KerbalSpace/Test.ks
@@ -2,13 +2,17 @@
 >'Please input something.'
 < a
 < b, 'Now input something else.\n'
-> 'You just said, '+a+" and " + b
+> 'You just said '+a+" and " + b
 --if 1:
 	--print('Hi!')
 	>'Bypassing works!'
 
-? True : >'hola': >'buenos dias'
+? True : >'This is true.': >'This is false.'
+? True: >'Single-line if-statements without "else" work too!'
 
+? 1==1
+	>'This is the beauty of KerbalSpace!'
+>'The conditional stops because there is no indent here.'
 
 <python>
 	print("Hello world!\nThis is embedded Python.")
--- KerbalSpace/test.py
+++ KerbalSpace/test.py
@@ -1,14 +1,19 @@
 print('Please input something.')
 a = input()
 b = input( 'Now input something else.\n')
-print('You just said, '+a+" and " + b)
+print('You just said '+a+" and " + b)
 if 1:
 	print('Hi!')
 	print('Bypassing works!')
 if  True :
-    print('hola')
+    print('This is true.')
 else:
-    print('buenos dias')
+    print('This is false.')
+if  True:
+    print('Single-line if-statements without "else" work too!')
+if  1==1:
+	print('This is the beauty of KerbalSpace!')
+print('The conditional stops because there is no indent here.')
 
 print("Hello world!\nThis is embedded Python.")
 a = input("Please input something.\n")
