aa140a858fde62768821e16eac0d2abae0b2755f
StardustGogeta <liam.gerst@gmail.com>
2016-04-27 16:37:42 -0400

    Attempting to make progress on Haskell

--- Haskell/FC4.hs
+++ Haskell/FC4.hs
@@ -1,4 +1,4 @@
 fc4 :: Integer -> [IO ()]
 check :: Integer -> Double -> IO ()
 fc4 input = map (check input) [1 .. sqrt (fromIntegral input)]
-check input divisor = if (fromIntegral input/divisor==0) then print(divisor, fromIntegral input/divisor) else print()
+check input divisor = if (fromIntegral input/divisor==0) then print divisor (fromIntegral input/divisor) else print ()
