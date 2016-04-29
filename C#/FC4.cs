bce924d35dad7c2152e6459c3ac3db776469a7b9
StardustGogeta <liam.gerst@gmail.com>
2016-04-29 17:35:13 -0400

    Creating a version of the FC4 in C#

---
+++ C#/FC4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            long n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine();
+            for (long x = 1; x <= Math.Floor(Math.Sqrt(n)); x++)
+                if (n % x == 0)
+                    Console.WriteLine(n / x + ", " + x);
+            Console.WriteLine();
+            Console.WriteLine("List complete.");
+        }
+    }
+}
