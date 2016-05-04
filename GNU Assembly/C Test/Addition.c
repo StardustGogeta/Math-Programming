d180ea4c0f52656bc8dc0cc787fef4c82095706b
StardustGogeta <liam.gerst@gmail.com>
2016-05-04 17:32:26 -0400

    Adding test assembly files and a Perl FC4

--- GNU Assembly/C Test/Addition.c
+++ GNU Assembly/C Test/Addition.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hello, world");
+	printf("%d",4+25);
 }
---
+++ Perl/FC4.pl
@@ -0,0 +1,6 @@
+use bignum;print "Enter a number.\n";
+$n = <STDIN>;
+for ($i=1;$i<=sqrt($n);$i++){
+if ($n % $i==0){
+print $i,", ",$n/$i,"\n";}}
+print "List complete.\n";
