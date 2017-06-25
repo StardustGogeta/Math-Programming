ef6a6045336ce583ae7ec6f401b7b2f8d901f09c
StardustGogeta <liam.gerst@gmail.com>
2017-06-25 19:32:28 -0400

    Slightly changing the FC5
    Now, the helper function is fully encapsulated within the function fc5,
    removing the possibility of misuse.

--- Haskell/FC5.hs
+++ Haskell/FC5.hs
@@ -2,8 +2,8 @@ import Data.List
 factors n = filter (\x -> rem n x == 0) [2..(floor (sqrt (fromIntegral n)))]
 findFactor n = if null (factors n) then n else head (factors n) 
 
-fc5 n = fc5b [1] n
-
-fc5b factors 1 = sort factors
-fc5b factors n = fc5b (nub (concat (map (\x -> [x,x*factor]) factors))) (quot n factor)
-	where factor = findFactor n
+fc5 n = f [1] n
+	where
+		f factors 1 = sort factors
+		f factors n = f (nub (concat (map (\x -> [x,x*factor]) factors))) (quot n factor)
+			where factor = findFactor n
