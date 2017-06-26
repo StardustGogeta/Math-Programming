5005406d72ec77ab48ea7cc27976b96c257c4e67
StardustGogeta <liam.gerst@gmail.com>
2017-06-25 22:06:58 -0400

    Replacing Haskell functions with concatMap
    I had forgotten about this useful function, but it does help save some
    characters from the files.

--- Haskell/FC4.hs
+++ Haskell/FC4.hs
@@ -1,2 +1,2 @@
 import Data.List
-fc4 a = sort (concat (map (\x -> [x, div a x]) (filter (\x -> rem a x == 0) [1..(floor (sqrt (fromIntegral a)))])))
+fc4 a = sort (concatMap (\x -> [x, div a x]) (filter (\x -> rem a x == 0) [1..(floor (sqrt (fromIntegral a)))]))
