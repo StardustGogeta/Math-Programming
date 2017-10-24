0a93e3121fbfc9694b85ebb80b9de33904a7984f
StardustGogeta <liam.gerst@gmail.com>
2017-10-24 18:00:02 -0400

    Adding a Java CES
    I am starting to grow a bit more comfortable towards Java, unfortunately. It's almost like Stockholm syndrome!

--- Java/FC4.java
+++ Java/FC4.java
@@ -4,17 +4,14 @@ class FC4 {
         System.out.println("What is the number?");
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
-        if (n > 0)
-        {
+        if (n > 0) {
             for (long d=1; d<Math.sqrt(n); d++)
-                if ((n % d) == 0)
-                {
+                if ((n % d) == 0) {
                     System.out.println(n / d + ", " + d);
                 }
         }
-        else
-        {
-        System.out.println("Please try again.");
+        else {
+            System.out.println("Try again.");
         }
     }
 }
