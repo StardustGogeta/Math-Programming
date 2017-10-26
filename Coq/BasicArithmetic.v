db1fc52181129196dea143ced60ddd60baf92353
StardustGogeta <liam.gerst@gmail.com>
2017-10-26 16:00:52 -0400

    Proving distribution over addition
    Importing Omega makes all of the trivial arithmetic (commutative and associative properties) so much easier.

---
+++ Coq/BasicArithmetic.v
@@ -0,0 +1,42 @@
+Require Import Bool.
+Require Import Setoid.
+Require Import Omega. (* This makes arithmetic relatively easy to handle*)
+
+Lemma zeroX_is_zero: (forall x, 0 * x = 0).
+Proof.
+  trivial.
+Qed.
+
+Lemma Xzero_is_zero: (forall x, x * 0 = 0).
+Proof.
+  intros.
+  omega.
+  (* Alternatively...
+  elim x.
+    trivial.
+    simpl.
+    trivial. *)
+Qed.
+
+Lemma subtractOne: (forall a b, S(a) = S(b) <-> a = b).
+Proof.
+  intros.
+  unfold iff.
+  refine (conj _ _). intro f. injection f. trivial.
+  intro. f_equal. exact H.
+Qed.
+
+Theorem distributive: (forall a x y, a*(x+y)=a*x+a*y).
+Proof.
+  intros.
+  elim a.
+    reflexivity.
+    elim x.
+      elim y.
+        trivial.
+        simpl. intros.
+        rewrite Xzero_is_zero.
+        trivial. intros. simpl.
+        simpl in H0. rewrite H0.
+        omega.
+Qed.
