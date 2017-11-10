2cb7b9fce5e1fd25ba3472263b604aee11e97f46
StardustGogeta <liam.gerst@gmail.com>
2017-11-09 22:44:34 -0500

    Adding the very beginnings of modular math in Coq
    This is difficult but doable with the SearchAbout tactic. I can't believe I haven't seen more references to it!

--- Coq/BasicArithmetic.v
+++ Coq/BasicArithmetic.v
@@ -1,7 +1,8 @@
 Require Import Bool.
 Require Import Setoid.
+Require Import Arith.
 Require Import Omega.
-(* This makes arithmetic relatively easy to handle *)
+(* Omega makes arithmetic relatively easy to handle *)
 
 Lemma zeroX_is_zero: (forall x, 0 * x = 0).
 Proof.
@@ -39,4 +40,36 @@ Proof.
         trivial. intros. simpl.
         simpl in H0. rewrite H0.
         omega.
+Qed.
+
+SearchAbout Nat.modulo.
+SearchAbout Nat.divide.
+
+Lemma y_divides_x_plus_y: forall x y, Nat.divide y (x+y) -> Nat.divide y x.
+Proof.
+  intros.
+  cut (Nat.divide y (x+y) -> Nat.divide y y -> Nat.divide y x).
+  intros.
+  exact (H0 H (Nat.divide_refl y)).
+  cut ((x + y) - y = x).
+  intros.
+  rewrite <- H0.
+  apply Nat.divide_sub_r.
+  exact H1. exact H2. omega.
+Qed.
+
+(* Nat.divide_add_r: forall n m p : nat,
+  Nat.divide n m -> Nat.divide n p -> Nat.divide n (m + p) *)
+Theorem evenPlusTwoIsEven: forall x, x mod 2 = 0 -> (S (S x)) mod 2 = 0.
+Proof.
+  intros.
+  rewrite Nat.mod_divide in H.
+  rewrite <- Nat.add_1_r.
+  rewrite <- Nat.add_1_r.
+  rewrite Nat.mod_divide.
+  cut (x + 1 + 1 = x + 2).
+  intros. rewrite H0.
+  apply Nat.divide_add_r.
+  exact H. apply Nat.divide_refl.
+  omega. omega. omega.
 Qed.
