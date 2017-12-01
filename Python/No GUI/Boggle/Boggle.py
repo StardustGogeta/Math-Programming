b6e37bc40fc2a2d44789d9d1255b505e81e72520
StardustGogeta <liam.gerst@gmail.com>
2017-12-01 17:52:48 -0500

    Adding Boggle solver
    That wasn't too bad.

---
+++ Python/No GUI/Boggle/Boggle.py
@@ -0,0 +1,53 @@
+txt = open("english_words.txt")
+english = list(filter(lambda x: len(x) < 18, txt.read().upper().splitlines()))
+txt.close()
+
+def findWordsStartingWith(s, words = english):
+    ls = list(filter(lambda x: x.startswith(s), words))
+    return ls
+
+def getAdjacentCoords(coords, path): # path consists of letters already used
+    y, x = coords
+    adj = []
+    if x > 0: adj.append((y, x-1))
+    if x < 3: adj.append((y, x+1))
+    if y > 0:
+        adj.append((y-1, x))
+        if x > 0: adj.append((y-1, x-1))
+        if x < 3: adj.append((y-1, x+1))
+    if y < 3:
+        adj.append((y+1, x))
+        if x > 0: adj.append((y+1, x-1))
+        if x < 3: adj.append((y+1, x+1))
+    return list(filter(lambda x: x not in path, adj))
+
+b = open("board.txt")
+boardTxt = b.read()
+b.close()
+board = [boardTxt[x*4:(x+1)*4] for x in range(4)] # Would have to be rewritten for Qu combo
+#print(board)
+
+def formWords(coords, board, path, tempWord = '', prevSubset = english):
+    formed = []
+    y, x = coords
+    tempWord += board[y][x]
+    path.append(coords)
+    if len(tempWord) > 2 and tempWord in english:
+        formed += [tempWord]
+    wordsSubset = findWordsStartingWith(tempWord, prevSubset)
+    if len(wordsSubset):
+        for c in getAdjacentCoords(coords, path):
+            formed += formWords(c, board, list(path), tempWord, wordsSubset)
+    return formed
+
+masterList = []
+for y in range(4):
+    for x in range(4):
+        print("Cell complete")
+        masterList += formWords((y, x), board, list()) # Removing list() causes path to break because of list wackiness
+
+masterList = sorted(list(set(masterList)))
+print(' '.join(masterList))
+out = open('out.txt', 'w')
+out.write(' '.join(masterList))
+out.close()
