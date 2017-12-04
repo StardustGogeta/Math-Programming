6cdae1c50e6309546403f38df20a0a1de4b123fe
StardustGogeta <liam.gerst@gmail.com>
2017-12-03 23:31:28 -0500

    Small Boggle optimizations
    This shaves over half a second off the previous times, in most cases.

--- Python/No GUI/Boggle/Boggle.py
+++ Python/No GUI/Boggle/Boggle.py
@@ -1,14 +1,13 @@
 from time import clock
 start = clock()
 txt = open("english_words.txt")
-english = list(filter(lambda x: len(x) < 18, txt.read().upper().splitlines()))
+english = list(x for x in txt.read().upper().splitlines() if 2 < len(x) < 18)
 txt.close()
 
 def findWordsStartingWith(s, words = english):
-    ls = list(filter(lambda x: x.startswith(s), words))
-    return ls
+    return list(x for x in words if x.startswith(s))
 
-def getAdjacentCoords(coords, path): # path consists of letters already used
+def getAdjacentCoords(coords): # path consists of letters already used
     y, x = coords
     adj = []
     if x > 0: adj.append((y, x-1))
@@ -21,7 +20,7 @@ def getAdjacentCoords(coords, path): # path consists of letters already used
         adj.append((y+1, x))
         if x > 0: adj.append((y+1, x-1))
         if x < 3: adj.append((y+1, x+1))
-    return list(filter(lambda x: x not in path, adj))
+    return adj
 
 b = open("board.txt")
 boardTxt = b.read()
@@ -29,27 +28,24 @@ b.close()
 board = [boardTxt[x*4:(x+1)*4] for x in range(4)] # Would have to be rewritten for Qu combo
 #print(board)
 
-def formWords(coords, board, path, tempWord = '', prevSubset = english):
-    formed = []
-    y, x = coords
-    tempWord += board[y][x]
-    path.append(coords)
-    if len(tempWord) > 2 and tempWord in prevSubset:
-        formed += [tempWord]
+def formWords(coords, board, path, master, tempWord = '', prevSubset = english):
+    tempWord += board[coords[0]][coords[1]]
     wordsSubset = findWordsStartingWith(tempWord, prevSubset)
     if len(wordsSubset):
-        for c in getAdjacentCoords(coords, path):
-            formed += formWords(c, board, list(path), tempWord, wordsSubset)
-    return formed
+        path.append(coords)
+        if len(tempWord) > 2 and tempWord in prevSubset:
+            master.add(tempWord)
+        for c in getAdjacentCoords(coords):
+            if c not in path:
+                formWords(c, board, list(path), master, tempWord, wordsSubset)
 
-masterList = []
+masterList = set()
 for y in range(4):
     for x in range(4):
-        masterList += formWords((y, x), board, list()) # Removing list() causes path to break because of list wackiness
-        print("Cell complete")
+        formWords((y, x), board, list(), masterList)
 
-masterList = sorted(list(set(masterList)))
-print(' '.join(masterList))
+masterList = sorted(masterList)
+print(str(len(masterList))+' '.join(masterList))
 out = open('out.txt', 'w')
 out.write(' '.join(masterList))
 out.close()
