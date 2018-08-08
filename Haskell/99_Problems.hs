2216991f3597365753c9b23ab77893b637f27beb
StardustGogeta <liam.gerst@gmail.com>
2018-08-08 00:20:14 -0400

    Adding more Haskell problems

--- Haskell/99_Problems.hs
+++ Haskell/99_Problems.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.List.Split
+import Data.Function
 
 p01 = last
 
@@ -15,13 +16,13 @@ p06 ls = ls == reverse ls
 
 -- p07 skipped
 
-p08 :: String -> String
+p08 :: Eq a => [a] -> [a]
 p08 = map head . group
 
-p09 :: String -> [String]
+p09 :: Eq a => [a] -> [[a]]
 p09 = group
 
-p10 :: String -> [(Int, Char)]
+p10 :: Eq a => [a] -> [(Int, a)]
 p10 = map enc . group
     where enc s = (length s, head s)
     
@@ -58,5 +59,40 @@ p19 ls n = take (length ls) $ drop (length ls + n) $ cycle ls
 
 p20 :: Int -> [a] -> (a, [a])
 p20 n ls = (ls !! n, take n ls ++ drop (n+1) ls) -- Intentionally off-by-one
+
+p21 :: a -> [a] -> Int -> [a]
+p21 a ls n = take n ls ++ a : drop n ls -- Intentionally off-by-one
+
+p22 :: Enum a => a -> a -> [a]
+p22 a b = [a..b]
+
+-- p23 skipped
+-- p24 skipped
+-- p25 skipped
+
+p26 :: Int -> [a] -> [[a]]
+p26 n ls = filter ((== n) . length) $ subsequences ls
+
+-- p27 skipped
+
+p28a :: [[a]] -> [[a]]
+p28a = sortBy (compare `on` length)
+
+p28b :: [[a]] -> [[a]]
+p28b = concat . p28a . groupBy ((==) `on` length) . p28a
+
+-- p29 and p30 do not exist
+
+p31 :: Integral a => a -> Bool
+p31 n = if n < 2 then False else let md x = mod n x /= 0 in all md [2..floor $ sqrt $ fromIntegral n]
+
+p32 :: Integral a => a -> a -> a
+p32 = gcd
+
+p33 :: Integral a => a -> a -> Bool
+p33 a b = (1 == ) $ gcd a b
+
+p34 :: Integral a => a -> Int
+p34 n = length $ filter (p33 n) [1..n]
 
 
