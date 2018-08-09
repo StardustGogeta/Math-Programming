00d28d761c8e1d4e8f1873d3e0b5387ac9a02ff5
StardustGogeta <liam.gerst@gmail.com>
2018-08-09 01:36:27 -0400

    Even more Haskell

--- Haskell/99_Problems.hs
+++ Haskell/99_Problems.hs
@@ -1,17 +1,24 @@
 import Data.List
 import Data.List.Split
 import Data.Function
+import Data.Bits
 
+p01 :: [a] -> a
 p01 = last
 
+p02 :: [a] -> a
 p02 = head . tail . reverse
 
+p03 :: [a] -> Int -> a
 p03 ls n = ls !! (n-1)
 
+p04 :: [a] -> Int
 p04 ls = length ls
 
+p05 :: [a] -> [a]
 p05 = reverse
 
+p06 :: Eq a => [a] -> Bool
 p06 ls = ls == reverse ls
 
 -- p07 skipped
@@ -38,6 +45,7 @@ p12 = concatMap dec
           dec (Single c) = [c]
 
 -- p13 skipped
+
 p14 :: [a] -> [a]
 p14 = concatMap (\x -> [x,x])
 
@@ -64,7 +72,7 @@ p21 :: a -> [a] -> Int -> [a]
 p21 a ls n = take n ls ++ a : drop n ls -- Intentionally off-by-one
 
 p22 :: Enum a => a -> a -> [a]
-p22 a b = [a..b]
+p22 = enumFromTo
 
 -- p23 skipped
 -- p24 skipped
@@ -90,9 +98,71 @@ p32 :: Integral a => a -> a -> a
 p32 = gcd
 
 p33 :: Integral a => a -> a -> Bool
-p33 a b = (1 == ) $ gcd a b
+p33 = ((1 ==) .) . gcd
 
 p34 :: Integral a => a -> Int
 p34 n = length $ filter (p33 n) [1..n]
 
+p35 :: Integral a => a -> [a]
+p35 n = if n == 1 then [] else let f = findFactor n in f : p35 (n `div` f)
+    where findFactor n = head $ filter ((0 ==) . (mod n))  [2..]
 
+p36 :: Integral a => a -> [(a, Int)]
+p36 = let pair ls = (head ls, length ls) in map pair . group . p35
+
+p37 :: Int -> Int
+p37 = product . map (\(x,y) -> (x-1)*x^(y-1)) . p36
+
+-- p38 := p37 is much faster than p34, for obvious reasons
+
+p39 :: (Integral a, Enum a) => a -> a -> [a]
+p39 = (filter p31 .) . enumFromTo
+
+p40 :: (Integral a, Enum a) => a -> (a, a)
+p40 n = head $ filter (\(_,y) -> p31 y) $ map (\x -> (x, n-x)) $ p39 0 n
+
+p41a :: (Integral a, Enum a) => a -> a -> [(a, a)]
+p41a = (map p40 .) . (filter ((0 ==) . (`mod` 2)) .) . enumFromTo
+
+p41b :: (Integral a, Enum a) => a -> a -> a -> [(a, a)]
+p41b a b n = filter (\(x,y) -> x > n && y > n) $ p41a a b
+
+-- p42, p43, p44, and p45 do not exist
+
+and' :: Bool -> Bool -> Bool
+and' = (&&)
+or' :: Bool -> Bool -> Bool
+or' = (||)
+nand' :: Bool -> Bool -> Bool
+nand' = (not .) . (&&)
+nor' :: Bool -> Bool -> Bool
+nor' = (not .) . (||)
+xor' :: Bool -> Bool -> Bool
+xor' = (/=)
+impl' :: Bool -> Bool -> Bool
+impl' = (&&) -- Not implemented
+equ' :: Bool -> Bool -> Bool
+equ' = (==)
+infixr 3 `and'`, `nand'`
+infixr 2 `or'`, `nor'`
+infixr 1 `impl'`, `equ'`, `xor'`
+
+-- Taken from https://stackoverflow.com/a/1959734/5732397 --
+decToBin :: Int -> [Bool]
+decToBin x = reverse $ map (\y -> x .&. y > 0) (map (2^) [0..floor $ logBase 2 $ fromIntegral x])
+decToBinFixedLen :: Int -> Int -> [Bool]
+decToBinFixedLen len x = reverse $ map (\y -> x .&. y > 0) (map (2^) [0..len-1])
+
+-- This has been generalized to n arguments, provide as a list in a lambda for f
+p46 :: Int -> ([Bool] -> Bool) -> IO ()
+p46 n f = sequence_ (map printLn (map (decToBinFixedLen n) [0..2^n-1]))
+    where
+        printLn arr = putStrLn (show $ arr ++ [f arr])
+
+-- p47 and p48 are completed by the function p46 above
+
+-- A gray code is one where it is n-digits wide and n+1 strings long
+-- Each new layer has the old one with zeros overlaid, then the reversed old with ones overlaid
+p49 :: Integral a => a -> [String]
+p49 1 = ["0", "1"]
+p49 n = map ('0':) (gray (n-1)) ++ map ('1':) (reverse $ gray (n-1))
