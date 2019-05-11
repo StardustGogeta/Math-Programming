b53179a36dc3c0be0f096eaa44bb7a087de59609
StardustGogeta <liam.gerst@gmail.com>
2019-05-11 02:12:55 -0400

    Fix factor calculator in C

--- C/Factor Calculator 4.c
+++ C/Factor Calculator 4.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-void main()
-{
-    long long n;
-	puts("What is your number?");
-	scanf("%lld",&n);
-	if (n>0)
-	{
-	    long long d;
-		for (d=1; d<=sqrt(n); d++)
-			if (n % d == 0)
-				printf("%lld, %lld\n",n/d,d);
-		puts("\nThe list of factors has been successfully generated.");
-	}
-	else
-		puts("\nSorry, I can't do that.");
+void main() {
+    unsigned long long n, d;
+    printf("What is your number?\n");
+    scanf("%llu", &n);
+    printf("\n");
+    if (n > 0) {
+        for (d = 1; d <= sqrt(n); d++)
+            if (n % d == 0)
+                printf("%llu, %llu\n", n / d , d);
+        printf("\nThe list of factor pairs has been successfully generated.\n");
+    }
+    else
+        printf("\nInvalid number!\n");
 }
