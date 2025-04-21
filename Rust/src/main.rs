e1a04a8f89d4316680c3382283fcadadf7c840ad
StardustGogeta <liam.gerst@gmail.com>
2025-04-21 12:31:23 -0400

    Update factor calculator with improvement for product of large primes

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashSet;
 
-
-fn findFactor(n : usize) -> usize {
-    for i in 2..(n as f64).sqrt().ceil() as usize {
+fn find_factor(n : u128, start : u128) -> u128 {
+    for i in start..(n as f64).sqrt().ceil() as u128 {
         if n % i == 0 {
             return i
         }
@@ -10,11 +9,12 @@ fn findFactor(n : usize) -> usize {
     n
 }
 
-fn findFactors(mut n : usize) -> HashSet<usize> {
+fn find_factors(mut n : u128) -> HashSet<u128> {
     let mut ret = HashSet::new();
     ret.insert(1);
+    let mut f = 2;
     while n > 1 {
-        let f = findFactor(n);
+        f = find_factor(n, f);
         n /= f;
         for i in ret.clone().into_iter() {
             ret.insert(f * i);
@@ -23,9 +23,20 @@ fn findFactors(mut n : usize) -> HashSet<usize> {
     ret
 }
 
-fn main() {
-    let x = 1000000000000111111;
-    let mut factors = findFactors(x).into_iter().collect::<Vec<_>>();
+fn print_factors(x : u128) {
+    let mut factors = find_factors(x).into_iter().collect::<Vec<_>>();
     factors.sort();
     println!("The factors of {x:?} are {factors:?}");
+}
+
+fn main() {
+    /*
+    for offset in 0..10 {
+        let x = 1000000000000111111 + offset;
+        printFactors(x);
+    }
+    */
+    // Product of large primes
+    let x : u128 = 101214161 * 101373101 * 101838101;
+    print_factors(x);
 }
